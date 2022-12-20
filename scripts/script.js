12b6f4fe17db0af345690896659ae6c611a9132b
Nikki612 <86887514+Nikki612@users.noreply.github.com>
2022-12-20 11:35:17 +0200

    noobie commit
    .

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const triggerTabList = document.querySelectorAll('#listElements button')
+triggerTabList.forEach(triggerEl => {
+  const tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', event => {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
