ff0a9f6464333277345708bfcfe4ab6b10481503
Nikki612 <86887514+Nikki612@users.noreply.github.com>
2023-01-04 15:26:52 +0200

    Draggable Buddy Across Canvas

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,92 @@
-const triggerTabList = document.querySelectorAll('#listElements button')
-triggerTabList.forEach(triggerEl => {
+window.onload = function()
+{
+  //--------------------------------------------START OF TABBING SECTION---------------------------------
+  const triggerTabList = document.querySelectorAll('#listElements button')
+  triggerTabList.forEach(triggerEl => {
   const tabTrigger = new bootstrap.Tab(triggerEl)
 
   triggerEl.addEventListener('click', event => {
     event.preventDefault()
     tabTrigger.show()
-  })
-})
+    })
+  });
+//--------------------------------------------END OF TABBING SECTION---------------------------------
+
+//--------------------------------------------START OF DRAG ACROSS CANVAS SECTION---------------------------------
+  var canvas, ctx;
+  var img = new Image();
+  var isDraggable = false;
+  
+  var currentX = 0;
+  var currentY = 0;
+
+  canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+  
+    currentX = canvas.width/2;
+    currentY = canvas.height/2;
+  
+    //See which image was pressed
+    const images = document.getElementsByTagName("img");
+
+    const ImgPressed = e => { 
+      console.log(e.target.id);
+      img.src=`./assets/${e.target.id}.png`;
+      img.onload = function() {
+        ProcessImg();
+      };
+      console.log("Image is pressed");
+    }
+
+    for (let image of images) {
+      image.addEventListener("click", ImgPressed);
+  }
+    
+  
+  function ProcessImg() {
+    MouseEvents();
+  
+    setInterval(function() {
+      Refresh();
+      DrawImg();
+    }, 1000/30);
+  }
+  function Refresh() {
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+  }
+  function MouseEvents() {
+    canvas.onmousedown = function(e) {
+  
+      var mouseX = e.pageX - this.offsetLeft;
+      var mouseY = e.pageY - this.offsetTop;
+  
+  
+      if (mouseX >= (currentX - img.width/2) &&
+          mouseX <= (currentX + img.width/2) &&
+          mouseY >= (currentY - img.height/2) &&
+          mouseY <= (currentY + img.height/2)) {
+        isDraggable = true;
+      }
+    };
+    canvas.onmousemove = function(e) {
+  
+      if (isDraggable) {
+        currentX = e.pageX - this.offsetLeft;
+        currentY = e.pageY - this.offsetTop;
+      }
+    };
+    canvas.onmouseup = function(e) {
+      isDraggable = false;
+    };
+    canvas.onmouseout = function(e) {
+      isDraggable = false;
+    };
+  }
+  function DrawImg() {
+    ctx.drawImage(img, currentX-(img.width/2), currentY-(img.height/2));
+};
+//--------------------------------------------END OF DRAG ON CANVAS SECTION---------------------------------
+
+};
+    
